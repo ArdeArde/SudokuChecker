595bf5583d8db6543b30d9ad1892ec87adabc740
Artturi <heikkilaartturi@gmail.com>
2023-11-21 23:20:32 +0200

    commit

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,10 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String ii = "41736982563215894795872431682543716979158643234691275828964357157329168416487529;";
+String iii = "-417369825632158-947958724316825437169791586432346912758289643571573291684164875293";
+String iv = "61369825432158947958724316825437169791586432346912758289643571573291684164875293";
+String V = "417369825432158947958724316425437169791586432386912758289643571573291684164875293";
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -23,6 +27,30 @@ SudokuVerifier v = new SudokuVerifier();
 		int a = v.verify(i);
 		assertEquals("incorrect string", a, -2);
 		
+	}
+	
+	@Test
+	public void testIncorrectString2() {
+		int a = v.verify(ii);
+		assertEquals("incorrect string", a, 1);
+	}
+	
+	@Test
+	public void testIncorrectString3() {
+		int a = v.verify(iii);
+		assertEquals("incorrect string", a, -1);
+	}
+	
+	@Test
+	public void testIncorrectString4() {
+		int a = v.verify(iv);
+		assertEquals("incorrect string", a, -3);
+	}
+	
+	@Test
+	public void testIncorrectString5() {
+		int a = v.verify(V);
+		assertEquals("incorrect string", a, -4);
 	}
 	
 	
